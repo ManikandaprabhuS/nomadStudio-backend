9d2fdc23102e684d61d55b88d3913d33bb4c8fcc
SivakumarMani <43170048+ManikandaprabhuS@users.noreply.github.com>
2026-08-31 13:20:30 +0530

    Update app.js
    deploy purpose it

--- invoice-backend/src/app.js
+++ invoice-backend/src/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://localhost:4200',
+    'https://nomadstudio.netlify.app/login'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
+
 app.use('/auth', require('./routes/auth.routes'));
 app.use('/api', require('./routes/protected.routes'));
 app.use('/api', require('./routes/user.routes'));
 app.use('/api', require('./routes/invoice.routes'));
 app.use('/api/expenses', require('./routes/expense.routes'));
-
 
 module.exports = app;
